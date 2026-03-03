016022c9be0986fdee9c84eae26e9aa03dd6b049
Rashmi <joshirashmi611@gmail.com>
2026-03-03 16:00:00 +0530

    feature updated vesrion

--- src/main/java/com/example/docker/HelloController.java
+++ src/main/java/com/example/docker/HelloController.java
@@ -13,7 +13,7 @@ public class HelloController {
 
   @GetMapping("/frontend")
    public String frontend() {
-       return "Frontend details pages update1";
+       return "Frontend details pages update  vesrion";
    }
 }
 
